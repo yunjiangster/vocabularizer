a24bf513fae585221313cc6e212d8539cf1b7999
Yunjiang Jiang <yunjiangster@gmail.com>
2015-01-11 15:41:39 -0800

    remove redundant code

--- pythoncode.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import cgi
-form = cgi.FieldStorage()
-param = form.getvalue("param","error")
-print param
--- server/pythoncode.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import cgi, cgitb
-cgitb.enable()
-form = cgi.FieldStorage()
-param = form.getvalue("param","error")
-print param
